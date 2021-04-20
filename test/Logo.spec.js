ed8f5bc5336c03912acc9034d6d170aa815fdafd
Илья Элланский <i.ellansky@rabota.ru>
2021-04-20 21:55:01 +0300

    Test

--- test/Logo.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import TableOfContents from '~/components/TableOfContents'
-
-describe('TableOfContents', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(TableOfContents)
-    expect(wrapper.vm).toBeTruthy()
-  })
-})
