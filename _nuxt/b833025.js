b5aa1c015bd43853c91f097be1ccf53e780325a6
Ellanskii <Ellanskii@users.noreply.github.com>
2021-04-20 19:10:38 +0000

    deploy: 0282bb80a77f058623ff105ed5eb3412080e12ec

---
+++ _nuxt/b833025.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{281:function(e,n,t){"use strict";t.r(n);var c={name:"HomePage"},l=t(43),component=Object(l.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("section",{staticClass:"section"})}),[],!1,null,null,null);n.default=component.exports}}]);
