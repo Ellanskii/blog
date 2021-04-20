4c52a3dc2cde595f03bbc4b768b4556572564e9e
Илья Элланский <i.ellansky@rabota.ru>
2021-04-21 02:13:27 +0300

    preview route fix

--- pages/index.vue
+++ pages/index.vue
@@ -20,16 +20,5 @@ export default {
         </li>
       </ul>
     </section>
-
-    <section>
-      <h2>Experiments</h2>
-      <ul>
-        <li>
-          <nuxt-link to="experiments/DynamicPreviewImage">
-            Dynamic preview image
-          </nuxt-link>
-        </li>
-      </ul>
-    </section>
   </main>
 </template>
