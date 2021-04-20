adf1bda182e490f6ef1bc79f35eb45ce0e7f945b
Илья Элланский <i.ellansky@rabota.ru>
2021-04-20 23:33:46 +0300

    Listing articles

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
-<template>
-  <section class="section"></section>
-</template>
-
 <script>
 export default {
   name: 'HomePage',
+
+  async asyncData({ $content, params, error }) {
+    const articles = await $content('articles').only(['title', 'slug']).fetch()
+
+    return { articles }
+  },
 }
 </script>
+
+<template>
+  <main>
+    <section>
+      <h2>Articles</h2>
+      <ul>
+        <li v-for="article in articles" :key="article.slug">
+          <NuxtLink :to="article.path">{{ article.title }}</NuxtLink>
+        </li>
+      </ul>
+    </section>
+  </main>
+</template>
